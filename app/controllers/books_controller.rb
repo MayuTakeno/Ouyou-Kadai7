b90b7eaf5ef4373c54bbb28685ab03339afb5a8e
EC2 Default User <ec2-user@ip-172-31-24-51.ec2.internal>
2022-06-02 04:42:36 +0000

    5thCommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,13 @@ class BooksController < ApplicationController
   before_action :correct_user, only: [:edit ]
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
   end
 
