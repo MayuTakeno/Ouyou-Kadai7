286968543df32574b55b96f858f3bc5a2ac43214
EC2 Default User <ec2-user@ip-172-31-24-51.ec2.internal>
2022-06-03 04:35:13 +0000

    12thCommit

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,40 +5,33 @@ class ChatsController < ApplicationController
     @user = User.find(params[:id])
     #user_roomsテーブルのuser_idがAさんのレコードのroom_idを配列で取得
     rooms = current_user.user_rooms.pluck(:room_id)
-    user_room = UserRoom.find_by(user_id: @user.id, room_id: rooms)
-    if user_room.nil?
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    unless user_rooms.nil?
+      @room = user_rooms.room
+    else
       @room = Room.new
       @room.save
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
-    else
-      @room = user_room.room
+      UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
   
   def create
-    @chat = Chat.new(chat_params)
-    respond_to do |format|
-      if @chat.save
-        format.html {redirect_to @chat}
-        format.js
-      else
-        format.html { render :show }
-        format.js { render :errors }
-      end
-    end
+    @chat = current_user.chats.new(chat_params)
+    render :validater unless @chat.save
   end
   
     private
     def chat_params
-      params.require(:chat).permit(:message, :room_id).merge(user_id: current_user.id)
+      params.require(:chat).permit(:message, :room_id)
     end
     
     def reject_non_related
       user = User.find(params[:id])
       unless current_user.following?(user) && user.following?(current_user)
+        redirect_to books_path
       end
     end
 end
