48af78023ce68e0fead6be6a47d028f9609687d8
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-06-07 10:03:14 +0000

    35thCommit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,18 @@
 class Group < ApplicationRecord
- has_many :group_users
- has_many :users, through: :group_users
+  has_one_attached :image
+  has_many :group_users, dependent: :destroy
+  has_many :users, through: :group_users
+  belongs_to :owner, class_name: 'User'
  
- validates :name, presence: true
- validates :introduction, presence: true
- attachment :image, destroy: false
+ 
+  validates :name, presence: true
+  validates :introduction, presence: true
+ 
+  def get_image
+    (image.attached?) ? image : 'no-image.jpg'
+  end
+ 
+  def is_owned_by?(user)
+    owned.id == user.id
+  end
 end
