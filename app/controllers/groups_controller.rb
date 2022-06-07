714620fbbafbd3d1be67a615e8b20270357ae154
MayuTakeno <zhenyouzhuye7@gmail.com>
2022-06-07 09:41:13 +0000

    35thCommit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,10 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
  
+  def new
+    @group = group.new
+  end
+  
   def index
     @book = Book.new
     @groups = Group.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   has_many :followers, through: :reverse_of_relationships, source: :following
   has_many :view_counts, dependent: :destroy
+  
 
   #検索方法分岐
   def self.looks(search, word)
